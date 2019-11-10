d9a194478533d8f91a79bdfc431c275588c6e2cd
Oshan Singh Karki <oshansinghkarki@Oshans-MacBook-Pro.local>
2019-11-09 23:29:18 -0800

    Added files given by dr. Chao

---
+++ Files/R_Files/3_code for template_Age.R
@@ -0,0 +1,17 @@
+ex <- readLines("template_Age.txt") # the template
+data <- read.csv("data.csv") # the csv file with the Age data (Age and age are different here)
+
+for (i in 2:nrow(data)){
+  ex1 <- gsub(pattern = "Example", replace = as.character(stage[i,2]), x = ex) # the name of the concept
+  ex1 <- gsub(pattern = "REPLACE1", replace = as.character(stage[i,5]), x = ex1) # broader
+  ex1 <- gsub(pattern = "REPLACE2", replace = as.character(stage[i,6]), x = ex1) # broaderTransitive
+  ex1 <- gsub(pattern = "REPLACE3", replace = as.character(stage[i,7]), x = ex1) # broaderTransitive
+  ex1 <- gsub(pattern = "REPLACE4", replace = as.character(stage[i-1,2]), x = ex1) # the concept above the present concept as the end of the present concept
+  ex1 <- gsub(pattern = "REPLACE5", replace = as.character(stage[i,4]), x = ex1) # bottom age of the concept
+  if(i == 2){
+    result <- ex1
+  }else{
+    result <- c(result,ex1)
+  }
+}
+write(result, "result.ttl")
